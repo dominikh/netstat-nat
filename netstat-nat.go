ed71bed64e68a52cbfd8abf0aa559a0d50306648
Dominik Honnef <dominikh@fork-bomb.org>
2012-11-06 20:34:27 +0100

    use posix flags

--- netstat-nat.go
+++ netstat-nat.go
@@ -4,7 +4,8 @@ import (
 	"github.com/dominikh/simple-router/conntrack"
 	"github.com/dominikh/simple-router/lookup"
 
-	"flag"
+	flag "github.com/ogier/pflag"
+
 	"fmt"
 	"net"
 	"os"
@@ -17,15 +18,15 @@ import (
 //       -N: display NAT box connection information (only valid with SNAT & DNAT)
 //       -v: print version
 
-var onlySNAT = flag.Bool("S", false, "Display only SNAT connections")
-var onlyDNAT = flag.Bool("D", false, "Display only DNAT connections")
-var onlyLocal = flag.Bool("L", false, "Display only local connections (originating from or going to the router)")
-var onlyRouted = flag.Bool("R", false, "Display only connections routed through the router")
-var noResolve = flag.Bool("n", false, "Do not resolve hostnames") // TODO resolve port names as well
-var noHeader = flag.Bool("o", false, "Strip output header")
-var protocol = flag.String("p", "", "Filter connections by protocol")
-var sourceHost = flag.String("s", "", "Filter by source IP")
-var destinationHost = flag.String("d", "", "Filter by destination IP")
+var onlySNAT = flag.BoolP("snat", "S", false, "Display only SNAT connections")
+var onlyDNAT = flag.BoolP("dnat", "D", false, "Display only DNAT connections")
+var onlyLocal = flag.BoolP("local", "L", false, "Display only local connections (originating from or going to the router)")
+var onlyRouted = flag.BoolP("routed", "R", false, "Display only connections routed through the router")
+var noResolve = flag.BoolP("no-resolve", "n", false, "Do not resolve hostnames") // TODO resolve port names as well
+var noHeader = flag.BoolP("no-header", "o", false, "Strip output header")
+var protocol = flag.StringP("protocol", "p", "", "Filter connections by protocol")
+var sourceHost = flag.StringP("source", "s", "", "Filter by source IP")
+var destinationHost = flag.StringP("destination", "d", "", "Filter by destination IP")
 
 func main() {
 	flag.Parse()
